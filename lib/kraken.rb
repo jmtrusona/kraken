534245810f0e1a5d9ec2f536e0660b02a1f7a7f7
John M <jm@trusona.com>
2020-03-21 07:35:09 -0700

    initial commit

---
+++ lib/kraken.rb
@@ -0,0 +1,6 @@
+require "kraken/version"
+
+module Kraken
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/kraken_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Kraken do
+  it "has a version number" do
+    expect(Kraken::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
